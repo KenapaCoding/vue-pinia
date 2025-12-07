9ae0c1730af01cef8527e6437faa2a3a0ce58dc9
ranggo pato <patoranggo@gmail.com>
2025-12-07 16:10:45 +0700

    patch

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,27 @@ import { useCounterStore } from './stores/counter';
 import { useCounterOptStore } from './stores/counterOpt';
 import { storeToRefs } from 'pinia';
 
-const counter = useCounterOptStore();
+const counter = useCounterStore();
 
-const {count,doubleCount} = storeToRefs(counter)
+const {count,mssg,doubleCount} = storeToRefs(counter)
 
-const {increment} = counter
+const {increment} = counter;
+
+function handleClick () {
+  counter.$patch((state)=> {
+    state.count++
+    state.mssg = "hello"
+  })
+}
 
 </script>
 
 <template>
   <div>
     <h3>count : {{ count }}</h3>
+    <p>mssg: {{ mssg }}</p>
     <h3>double count : {{ doubleCount }}</h3>
-    <button @click="increment">Increse</button>
+    <button @click="handleClick">Increse</button>
   </div>
 </template>
 
